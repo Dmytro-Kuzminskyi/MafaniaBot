7a9f92a68c48766b5f63667d11fabe286f49ca9a
Dima <dimahol1996@gmail.com>
2021-03-24 18:14:13 +0100

    LeftChatMemberHandler, NewChatMemberHandler true async

--- Handlers/LeftChatMemberHandler.cs
+++ Handlers/LeftChatMemberHandler.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using MafaniaBot.Models;
+using System.Collections.Generic;
 using MafaniaBot.Abstractions;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -10,127 +10,64 @@ using StackExchange.Redis;
 
 namespace MafaniaBot.Handlers
 {
-    public class LeftChatMemberHandler : Entity<Message>
-    {
-        public override bool Contains(Message message)
-        {
-            if (message.Chat.Type == ChatType.Private || message.Chat.Type == ChatType.Channel)
-                return false;
+	public class LeftChatMemberHandler : Entity<Message>
+	{
+		public override bool Contains(Message message)
+		{
+			if (message.Chat.Type == ChatType.Private || message.Chat.Type == ChatType.Channel)
+				return false;
 
-            return (message.LeftChatMember != null) ? true : false;
-        }
+			return (message.LeftChatMember != null) ? true : false;
+		}
 
-        public override async Task Execute(Message message, ITelegramBotClient botClient, IConnectionMultiplexer redis)
-        {
-            try
-            {
-                long chatId = message.Chat.Id;
-                User member = message.LeftChatMember;
+		public override async Task Execute(Message message, ITelegramBotClient botClient, IConnectionMultiplexer redis)
+		{
+			try
+			{
+				var tokenSource = new CancellationTokenSource();
+				long chatId = message.Chat.Id;
+				User member = message.LeftChatMember;
+				string msg = null;
 
-                Logger.Log.Debug($"LeftChatMember HANDLER triggered in #chatId={chatId} #memberId={member.Id}");
+				Logger.Log.Debug($"LeftChatMember HANDLER triggered: #chatId={chatId} left member #userId={member.Id}");
 
-                string msg = null;
+				if (member.Id.Equals(botClient.BotId))
+				{
+					IDatabaseAsync db = redis.GetDatabase();
+					var dbTask0 = db.SetRemoveAsync(new RedisKey("MyGroups"), new RedisValue(chatId.ToString()));
+					var dbTask1 = db.KeyDeleteAsync(new RedisKey($"AskParticipants:{chatId}"));
+					var tasks = new List<Task> { dbTask0, dbTask1 };
+					await Task.WhenAll(tasks);
+					return;
+				}
 
-                if (member.Id.Equals(botClient.BotId))
-                {
-                    try
-                    {
-                        IDatabaseAsync db = redis.GetDatabase();
+				if (!member.IsBot)
+				{
+					int userId = member.Id;
+					string firstname = member.FirstName;
+					string lastname = member.LastName;
+					string mention = Helper.GenerateMention(userId, firstname, lastname);
+					msg = mention + ", покинул(а) чат 😕";
+					IDatabase db = redis.GetDatabase();
+					var dbTask = db.SetRemoveAsync(new RedisKey($"AskParticipants:{chatId}"), new RedisValue(userId.ToString()));
+					var token = tokenSource.Token;
+					var messageTask = botClient.SendTextMessageAsync(chatId, msg, parseMode: ParseMode.Html, 
+							cancellationToken: token);
+					var tasks = new List<Task> { dbTask, messageTask };
 
-                        var key = new RedisKey("MyGroups");
-                        var value = new RedisValue(chatId.ToString());
+					if (!dbTask.IsCompletedSuccessfully)
+					{
+						tokenSource.Cancel();
+					}
 
-                        await db.SetRemoveAsync(key, value);
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.Log.Error("LeftChatMember HANDLER Error while processing Redis.MyGroups", ex);
-                    }
-                    /*using (var db = new MafaniaBotDBContext())
-                    {
-                        var recordsetParticipants = db.AskAnonymousParticipants
-                                .OrderBy(r => r.ChatId)
-                                .Where(r => r.ChatId.Equals(chatId));
-
-                        if (recordsetParticipants != null)
-                        {
-                            db.RemoveRange(recordsetParticipants);
-                        }
-
-                        var recordsetPendingQuestions = db.PendingAnonymousQuestions
-                                .OrderBy(r => r.ChatId)
-                                .Where(r => r.ChatId.Equals(chatId));
-
-                        if (recordsetPendingQuestions != null)
-                        {
-                            db.RemoveRange(recordsetPendingQuestions);
-                        }
-
-                        var recordsetPendingAnswers = db.PendingAnonymousAnswers
-                                .OrderBy(r => r.ChatId)
-                                .Where(r => r.ChatId.Equals(chatId));
-
-                        if (recordsetPendingAnswers != null)
-                        {
-                            db.RemoveRange(recordsetPendingAnswers);
-                        }
-
-                        var recordsetQuestions = db.AnonymousQuestions
-                                .OrderBy(r => r.ChatId)
-                                .Where(r => r.ChatId.Equals(chatId));
-
-                        if (recordsetQuestions != null)
-                        {
-                            db.RemoveRange(recordsetQuestions);
-                        }
-
-                        await db.SaveChangesAsync();*/
-                }
-                if (!member.IsBot)
-                {
-                    int userId = member.Id;
-                    string firstname = member.FirstName;
-                    string lastname = member.LastName;
-
-                    try
-                    {
-                        using (var db = new MafaniaBotDBContext())
-                        {
-                            var record = db.AskAnonymousParticipants
-                                    .OrderBy(r => r.ChatId)
-                                    .Where(r => r.ChatId.Equals(chatId))
-                                    .Where(r => r.UserId.Equals(userId))
-                                    .FirstOrDefault();
-
-                            if (record != null)
-                            {
-                                Logger.Log.Debug($"LeftChatMember HANDLER Delete record: (#id={record.Id} #chatId={record.ChatId} #userId={record.UserId}) from db.AskAnonymousParticipants");
-
-                                db.Remove(record);
-                                await db.SaveChangesAsync();
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.Log.Error("LeftChatMember HANDLER Error while processing db.AskAnonymousParticipants", ex);
-                    }
-
-                    string mention = lastname != null ?
-                                $"<a href=\"tg://user?id={userId}\">" + Helper.ConvertTextToHtmlParseMode(firstname) + " " + Helper.ConvertTextToHtmlParseMode(lastname) + "</a>" :
-                                $"<a href=\"tg://user?id={userId}\">" + Helper.ConvertTextToHtmlParseMode(firstname) + "</a>";
-
-                    msg = mention + ", покинул Ханство!";
-
-                    Logger.Log.Debug($"LeftChatMember HANDLER SendTextMessage #chatId={chatId} #msg={msg}");
-
-                    await botClient.SendTextMessageAsync(chatId, msg, parseMode: ParseMode.Html);
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Log.Error("LeftChatMember HANDLER ---", ex);
-            }
-        }
-    }
+					await Task.WhenAll(tasks);
+					tokenSource.Dispose();
+				}
+			}
+			catch (Exception ex)
+			{
+				Logger.Log.Error("LeftChatMember HANDLER ---", ex);
+			}
+		}
+	}
 }
--- Handlers/NewChatMemberHandler.cs
+++ Handlers/NewChatMemberHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using MafaniaBot.Abstractions;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -8,76 +10,68 @@ using StackExchange.Redis;
 
 namespace MafaniaBot.Handlers
 {
-    public class NewChatMemberHandler : Entity<Message>
-    {
-        public override bool Contains(Message message)
-        {
-            if (message.Chat.Type == ChatType.Private || message.Chat.Type == ChatType.Channel)
-                return false;
+	public class NewChatMemberHandler : Entity<Message>
+	{
+		public override bool Contains(Message message)
+		{
+			if (message.Chat.Type == ChatType.Private || message.Chat.Type == ChatType.Channel)
+				return false;
 
-            return (message.NewChatMembers != null) ? true : false;
-        }
+			return (message.NewChatMembers != null) ? true : false;
+		}
 
-        public override async Task Execute(Message message, ITelegramBotClient botClient, IConnectionMultiplexer redis)
-        {
-            try
-            {
-                long chatId = message.Chat.Id;
-                User user = message.NewChatMembers[0];
-                string msg = null;
-                
-                Logger.Log.Debug($"NewChatMember HANDLER triggered in #chatId={chatId} #memberId={user.Id}");
+		public override async Task Execute(Message message, ITelegramBotClient botClient, IConnectionMultiplexer redis)
+		{
+			try
+			{
+				var tokenSource = new CancellationTokenSource();
+				long chatId = message.Chat.Id;
+				User user = message.NewChatMembers[0];
+				string msg = null;
 
-                if (user.Id.Equals(botClient.BotId))
-                {
-                    try
-                    {
-                        IDatabaseAsync db = redis.GetDatabase();
+				Logger.Log.Debug($"NewChatMember HANDLER triggered: #chatId={chatId} new member #userId={user.Id}");
 
-                        var key = new RedisKey("MyGroups");
-                        var value = new RedisValue(chatId.ToString());
+				if (user.Id.Equals(botClient.BotId))
+				{
+					msg =
+						"<b>Общие команды</b>\n" +
+						"/weather [city] — узнать текущую погоду.\n" +
+						"/ask — задать анонимный вопрос.\n\n" +
+						"<b>Команды группового чата</b>\n" +
+						"/askmenu — меню анонимных вопросов.";
 
-                        await db.SetAddAsync(key, value);
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.Log.Error("NewChatMember HANDLER Error while processing Redis.MyGroups", ex);
-                    }
+					IDatabaseAsync db = redis.GetDatabase();
+					var dbTask = db.SetAddAsync(new RedisKey("MyGroups"), new RedisValue(chatId.ToString()));
+					var token = tokenSource.Token;
+					var messageTask = botClient.SendTextMessageAsync(chatId, msg, parseMode: ParseMode.Html, 
+							cancellationToken: token);
+					var tasks = new List<Task> { dbTask, messageTask };
 
-                    msg =
-                        "<b>Общие команды</b>\n" +
-                        "/weather [city] — узнать текущую погоду.\n\n" +
-                        "<b>Команды группового чата</b>\n" +
-                        "/askmenu — меню анонимных вопросов.";
+					if (!dbTask.IsCompletedSuccessfully)
+					{
+						tokenSource.Cancel();
+					}
 
-                    Logger.Log.Debug($"NewChatMember HANDLER SendTextMessage #chatId={chatId} #msg={msg}");
+					await Task.WhenAll(tasks);
+					tokenSource.Dispose();
+					return;
+				}
 
-                    await botClient.SendTextMessageAsync(chatId, msg, parseMode: ParseMode.Html);
+				string firstname = user.FirstName;
+				string lastname = user.LastName;
+				int userId = user.Id;
 
-                    return;
-                }
-
-                string firstname = user.FirstName;
-                string lastname = user.LastName;
-                int userId = user.Id;
-
-                if (!user.IsBot)
-                {
-                    string mention = lastname != null ?
-                        $"<a href=\"tg://user?id={userId}\">" + Helper.ConvertTextToHtmlParseMode(firstname) + " " + Helper.ConvertTextToHtmlParseMode(lastname) + "</a>" :
-                        $"<a href=\"tg://user?id={userId}\">" + Helper.ConvertTextToHtmlParseMode(firstname) + "</a>";
-
-                    msg = mention + ", добро пожаловать в Ханство!";
-
-                    Logger.Log.Debug($"NewChatMember HANDLER SendTextMessage #chatId={chatId} #msg={msg}");
-
-                    await botClient.SendTextMessageAsync(chatId, msg, parseMode: ParseMode.Html);
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Log.Error("NewChatMember HANDLER ---", ex);
-            }
-        }
-    }
+				if (!user.IsBot)
+				{
+					string mention = Helper.GenerateMention(userId, firstname, lastname);
+					msg = mention + ", добро пожаловать 😊";
+					await botClient.SendTextMessageAsync(chatId, msg, parseMode: ParseMode.Html);
+				}
+			}
+			catch (Exception ex)
+			{
+				Logger.Log.Error("NewChatMember HANDLER ---", ex);
+			}
+		}
+	}
 }
